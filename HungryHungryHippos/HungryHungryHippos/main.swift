810a9b0e9af611d056a65293638a5255085fb958
Hector L <hectoremil21@hotmail.com>
2020-04-08 18:49:29 -0400

    nvm I figured it out lol.

--- HungryHungryHippos/HungryHungryHippos/main.swift
+++ HungryHungryHippos/HungryHungryHippos/main.swift
@@ -41,27 +41,30 @@ class Developer {
     
     // MARK: - Methods
     
-    func think(_ id: Int) {
+    func think(_ id: Int, _ waiter: DispatchSemaphore) {
+        waiter.wait()
+        print("Dev\(id) requested the waiter.")
         leftSpoon.pickUp()
         print("Dev\(id) picked up left spoon.")
         rightSpoon.pickUp()
         print("Dev\(id) picked up right spoon.")
-        return
     }
     
-    func eat(_ id: Int) {
+    func eat(_ id: Int, _ waiter: DispatchSemaphore) {
         print("Dev\(id) about to eat some good stuff.")
         usleep(useconds_t.random(in: 0...1000000))
         rightSpoon.putDown()
         print("Dev\(id) put down left spoon.")
         leftSpoon.putDown()
         print("Dev\(id) put down right spoon.")
+        waiter.signal()
+        print("Dev\(id) gave the utensils back to the waiter.")
     }
     
-    func run(_ id: Int) {
+    func run(_ id: Int, _ waiter: DispatchSemaphore) {
         while true {
-            think(id)
-            eat(id)
+            think(id, waiter)
+            eat(id, waiter)
         }
     }
     
@@ -93,7 +96,6 @@ var developers = [dev1, dev2, dev3, dev4]
 let waiter = DispatchSemaphore(value: 4)
 
 DispatchQueue.concurrentPerform(iterations: 4) { i in
-    waiter.wait()
-    developers[i].run(i+1)
-    waiter.signal()
+    developers[i].run(i+1, waiter)
 }
+
